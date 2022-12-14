31e61b918ed3ff6bd2f3b73a39bbf521c55295ba
Jaap <jaap@mrproper.dev>
2022-12-14 14:29:36 +0100

    Update tree-tier-calculator.ts

--- src/util/tree-tier-calculator.ts
+++ src/util/tree-tier-calculator.ts
@@ -7,12 +7,12 @@ export function calculateTreeTierImage(size: number): string {
   if (size < 80) return "https://i.imgur.com/0jkawPy.png";
   if (size < 110) return "https://i.imgur.com/NBK1biD.jpg";
   if (size < 145) return "https://i.imgur.com/o3DYowJ.jpg";
-  if (size < 165) return "https://i.imgur.com/Y2P6t8G.jpg";
-  if (size < 185) return "https://i.imgur.com/nJn7BZY.jpg";
-  if (size < 205) return "https://i.imgur.com/8oTigep.jpg";
-  if (size < 225) return "https://i.imgur.com/j0sOYGV.jpg";
-  if (size < 235) return "https://i.imgur.com/pBAhFIX.jpg";
-  if (size < 255) return "https://i.imgur.com/cOWaOLA.jpg";
-  if (size < 295) return "https://i.imgur.com/nw8PZXM.jpg";
+  if (size < 185) return "https://i.imgur.com/Y2P6t8G.jpg";
+  if (size < 230) return "https://i.imgur.com/nJn7BZY.jpg";
+  if (size < 280) return "https://i.imgur.com/8oTigep.jpg";
+  if (size < 335) return "https://i.imgur.com/j0sOYGV.jpg";
+  if (size < 395) return "https://i.imgur.com/pBAhFIX.jpg";
+  if (size < 455) return "https://i.imgur.com/cOWaOLA.jpg";
+  if (size < 520) return "https://i.imgur.com/nw8PZXM.jpg";
   return "https://i.imgur.com/nw8PZXM.jpg";
 }
