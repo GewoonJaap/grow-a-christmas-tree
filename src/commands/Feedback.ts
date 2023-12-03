9aa5ec34815659d05c4f43f683a628577594c1f0
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2023-12-03 17:19:32 +0100

    Create Feedback.ts

---
+++ src/commands/Feedback.ts
@@ -0,0 +1,38 @@
+import {
+  EmbedBuilder,
+  ISlashCommand,
+  MessageBuilder,
+  SlashCommandBuilder,
+  SlashCommandContext,
+  SlashCommandStringOption
+} from "interactions.ts";
+import { FeedbackPost, postFeedback } from "../util/postFeedback";
+
+const builder = new SlashCommandBuilder(
+  "feedback",
+  "Got some feedback? Don't hesitate to submit it to us :)"
+).addStringOption(new SlashCommandStringOption("content", "The feedback you want to submit").setRequired(true));
+
+builder.setDMEnabled(false);
+
+export class Feedback implements ISlashCommand {
+  public builder = builder;
+
+  public handler = async (ctx: SlashCommandContext): Promise<void> => {
+    const feedback: FeedbackPost = {
+      content: ctx.options.get("content")?.value as string,
+      sendingServer: ctx.interaction.guild_id ?? "DM",
+      sendingUser: ctx.user.username
+    };
+
+    postFeedback(feedback);
+
+    return ctx.reply(
+      new MessageBuilder().addEmbed(
+        new EmbedBuilder().setTitle(`Thanks for submitting your feedback! We'll look into it.`)
+      )
+    );
+  };
+
+  public components = [];
+}
