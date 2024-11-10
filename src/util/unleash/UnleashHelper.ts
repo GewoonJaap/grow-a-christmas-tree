939aaa47a332581add0d26e2da291c332209698e
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2024-11-10 16:37:51 +0100

    Add unleash

---
+++ src/util/unleash/UnleashHelper.ts
@@ -0,0 +1,30 @@
+import { ButtonContext, SlashCommandContext } from "interactions.ts";
+import { Context, initialize } from "unleash-client";
+
+export const unleash = initialize({
+  url: process.env.UNLEASH_URL ?? "http://unleash-web:4242/api",
+  appName: "christmas-tree-bot",
+  environment: "production",
+  customHeaders: { Authorization: process.env.UNLEASH_TOKEN ?? "" }
+});
+
+export class UnleashHelper {
+  static getUnleashContext(ctx: SlashCommandContext | ButtonContext | ButtonContext<never>): Context {
+    return {
+      userId: ctx.game?.id ?? ctx.user.id,
+      properties: {
+        hasAiAccess: (ctx.game?.hasAiAccess ?? false).toString(),
+        size: ctx.game?.size ?? 0,
+        hasSuperThirsty: (ctx.game?.superThirsty ?? false).toString(),
+        contributorsAmount: ctx.game?.contributors.length ?? 0
+      }
+    };
+  }
+  static isEnabled(
+    name: string,
+    ctx: SlashCommandContext | ButtonContext | ButtonContext<never>,
+    fallbackValue = false
+  ): boolean {
+    return unleash.isEnabled(name, this.getUnleashContext(ctx), fallbackValue);
+  }
+}
