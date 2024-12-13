aa4c8b5fe9aa72ced175dbdc6e18bc2e20d048bf
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2024-12-13 18:26:30 +0100

    migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,4 @@
-import { migrateUnlockedTreeStyles } from "./migrateUnlockedTreeStyles";
-
 export async function runMigrations(): Promise<void> {
   console.log("Running migrations...");
-  await migrateUnlockedTreeStyles();
   console.log("Migrations completed.");
 }
