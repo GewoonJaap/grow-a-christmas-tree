91b9040507814818f13fafa7079fd3704cec98b6
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2024-12-15 09:57:05 +0100

    entitlment type

--- src/util/types/discord/DiscordTypeExtension.ts
+++ src/util/types/discord/DiscordTypeExtension.ts
@@ -3,12 +3,23 @@ export interface Entitlement {
   sku_id: string;
   application_id: string;
   user_id?: string;
-  type: number;
+  type: EntitlementType;
   deleted: boolean;
   starts_at?: string;
   ends_at?: string;
   guild_id?: string;
   consumed?: boolean;
+}
+
+export enum EntitlementType {
+  PURCHASE = 1,
+  PREMIUM_SUBSCRIPTION = 2,
+  DEVELOPER_GIFT = 3,
+  TEST_MODE_PURCHASE = 4,
+  FREE_PURCHASE = 5,
+  USER_GIFT = 6,
+  PREMIUM_PURCHASE = 7,
+  APPLICATION_SUBSCRIPTION = 8
 }
 
 export enum EntitlementType {
